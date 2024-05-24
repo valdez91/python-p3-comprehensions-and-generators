af6bad153a933ac6a95d422dc172d7d9cf7b74bc
Victor Kemboi <vickemtoo97@gmail.com>
2024-05-24 10:16:00 +0300

    comprehensions and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
+#Function takes a list of numbers
 def return_evens(num_list):
-    pass
+    return [v for v in num_list if v%2==0] #returns a new list containg only eve numbers
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list] #return new list containing same strings but with an exclamation mark added on each at the end
